4cbd5a33223db869c9f17a4f6b1b637a084bb4fe
VadimChek <wadim199602@gmail.com>
2018-04-21 19:48:33 +0700

    init

---
+++ backend/config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=test',
+    'username' => 'chvs',
+    'password' => '123',
+    'charset' => 'utf8',
+];
